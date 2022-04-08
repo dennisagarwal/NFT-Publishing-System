5584b8b4a9f13f19d6cbec44cda3c2ee91a3f5f8
Deepak Agarwal <47721949+dennisagarwal@users.noreply.github.com>
2022-04-07 21:09:48 -0400

    frontend login page console log worked

--- Project2-Team6-Frontend/src/app/login/login.component.ts
+++ Project2-Team6-Frontend/src/app/login/login.component.ts
@@ -5,29 +5,29 @@ import { AuthServiceService } from '../auth-service.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent  implements OnInit {
-formGroup!:FormGroup;
-  constructor(private authService:AuthServiceService) { }
+export class LoginComponent implements OnInit {
+  formGroup!: FormGroup;
+  constructor(private authService: AuthServiceService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): any {
+    this.initForm();
   }
-  initForm(){
-this.formGroup = new FormGroup({
-  email: new FormControl('', [Validators.required]),
-  password: new FormControl('',[Validators.required])
-})
+  initForm() {
+    this.formGroup = new FormGroup({
+      "username": new FormControl('', [Validators.required]),
+      "password": new FormControl('', [Validators.required]),
+    });
   }
-  loginProcess(){
-if(this.formGroup.valid){
-this.authService.login(this.formGroup.value).subscribe(result=>{
-if(result.success){
-  console.log(result);
-  alert(result.message);
-}
-})
-}
+  loginProcess() {
+    if (this.formGroup.valid) {
+      this.authService.login(this.formGroup.value).subscribe((result) => {
+        if (result.userRole=="manager"||result.userRole=="employer") {
+          console.log(result);
+          alert(result);
+        }
+      });
+    }
   }
-
 }
