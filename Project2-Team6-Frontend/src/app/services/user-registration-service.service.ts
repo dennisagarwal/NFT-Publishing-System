12240168b4f1694fbfd60fb619c8becb5e82ae89
Devathidhan <devathidhans@gmail.com>
2022-04-11 18:38:50 -0400

    Commit the regitation page

---
+++ Project2-Team6-Frontend/src/app/services/user-registration-service.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, Subject } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserRegistrationServiceService {
+  registrationSubject: Subject<string> = new Subject<string>();
+
+  constructor(private http: HttpClient) { }
+
+  apiUrl = "http://localhost:9090/users";
+
+   getPostUserData(username:string, password:string) {
+    const userRegInfo={"username": username, "password": password }
+    alert("beforePost");
+   this.http.post(this.apiUrl, userRegInfo,{'observe':'response'}).subscribe(
+    res => {
+      this.registrationSubject.next("User registered successfully!!!");
+      alert(res);
+    }
+  , err => {
+    //this.registrationSubject.next("Unable to create a user...");
+    alert("failure");
+      const errorMessage = err.message;
+      debugger;
+      this.registrationSubject.next(errorMessage); // Publish information to the loginErrorSubject
+  })
+  }
+
+
+}
