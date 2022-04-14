21f34d8274c70ce444d856432d8b395415733ba6
George Balch <george089@revature.net>
2022-04-13 17:40:37 -0700

    uploadImage

--- NFT-Publishing-System-Frontend/src/app/services/userimages.service.ts
+++ NFT-Publishing-System-Frontend/src/app/services/userimages.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { initializeApp } from 'firebase/app';
+import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBh5grkgbDI9H_tvHbrxMRpGfAKfIGQCNw",
+  authDomain: "stone-citizen-233922.firebaseapp.com",
+  projectId: "stone-citizen-233922",
+  storageBucket: "stone-citizen-233922.appspot.com",
+  messagingSenderId: "210489951771",
+  appId: "1:210489951771:web:58f69b23aa63e1b766dabe"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const storage = getStorage(firebaseApp);
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserimagesService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  // To generate image name, use username and specific name of file 
+  // combined to create unique filename 
+  public uploadImage(image: File) {
+    const imageRef = ref(storage, image.name);  
+    
+    uploadBytes(imageRef, image).then((snapshot) => {
+      console.log('Upload', snapshot.totalBytes, 'bytes.');
+      getDownloadURL(snapshot.ref).then(url => {
+        console.log('File available at', url);
+      });
+    }).catch(error => {
+      console.error('Upload failed:', error);
+    });
+  } 
 }
