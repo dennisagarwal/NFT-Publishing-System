c1d51120616c55c8e416058e2da5566b0b6c7ad8
Deepak Agarwal <47721949+dennisagarwal@users.noreply.github.com>
2022-04-15 02:04:43 -0400

    version1 for card in user-profile

--- NFT-Publishing-System-Frontend/src/app/components/user/user.component.ts
+++ NFT-Publishing-System-Frontend/src/app/components/user/user.component.ts
@@ -4,6 +4,8 @@ import { LoginService } from 'src/app/services/login.service';
 import { Router } from '@angular/router';
 import { UserInfo } from 'src/app/models/user-info';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { formatDate } from '@angular/common';
+import {faker}  from '@faker-js/faker';
 
 @Component({
   selector: 'app-user',
@@ -11,14 +13,18 @@ import { NavbarComponent } from '../navbar/navbar.component';
   styleUrls: ['./user.component.css'],
 })
 export class UserComponent implements OnInit {
-  id:any
-  username: any;
+  id!: number;
+  username!: string;
   password: any;
   ethAddress: any;
-
+  myDate!: any;
+  fakeImage!:string;
+  fakePhone!:string;
 
   constructor(private loginService: LoginService, private router: Router) {
-
+    this.myDate = formatDate(new Date(), 'yyyy/MM/dd  hh:mm', 'en');
+    this.fakeImage = faker.image.avatar();
+    this.fakePhone =faker.phone.phoneNumber()
   }
 
   ngOnInit(): void {
